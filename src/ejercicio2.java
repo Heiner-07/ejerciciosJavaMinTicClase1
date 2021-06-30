e7bb7b397d11aaaeb8882db971c6fc3688dccae3
Heiner-07 <heicufi@gmail.com>
2021-06-29 23:55:48 -0500

    Quitar tilde

--- src/ejercicio2.java
+++ src/ejercicio2.java
@@ -8,7 +8,7 @@ public class ejercicio2 {
         int numero = sc.nextInt();
 
         int digitos = numeroDigitos(numero);
-        System.out.print("El número " + numero + " tiene " + digitos + " cifra(s)");
+        System.out.print("El numero " + numero + " tiene " + digitos + " cifra(s)");
     }
 
     public static int numeroDigitos(int numero) {
